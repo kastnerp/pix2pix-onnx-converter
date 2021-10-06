aa5555e647ca5da925b4389190482bb59476e6f6
Patrick Kastner <patrick.kastner@gmail.com>
2021-10-06 16:01:30 -0400

    Init

---
+++ to_onnx.py
@@ -0,0 +1,27 @@
+import os
+from options.test_options import TestOptions
+from data import create_dataset
+from models import create_model
+
+from util.visualizer import save_images
+from util import html
+
+import torch
+import torchvision.models as models
+
+import time
+
+if __name__ == '__main__':
+    opt = TestOptions().parse()  # get test options
+    # hard-code some parameters for test
+    opt.num_threads = 0   # test code only supports num_threads = 1
+    opt.batch_size = 1    # test code only supports batch_size = 1
+    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
+    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
+    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    #dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    model = create_model(opt)      # create a model given opt.model and other options
+    model.setup_saveONNX(opt)               # regular setup: load and print networks; create schedulers
+
+
+
